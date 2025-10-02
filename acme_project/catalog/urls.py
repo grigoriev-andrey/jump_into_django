2cb4c4f078ce533d23defb9c6ba4a1d6d6650e01
Andrey Grigoriev <andreygrigoriev3112@gmail.com>
2025-10-02 01:25:06 +0100

    download this

--- acme_project/catalog/urls.py
+++ acme_project/catalog/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<slug:category>/', views.product_category),
     path('', views.product_list),
     path('1/', views.product_detail),
     path('2/', views.product_detail),
--- acme_project/catalog/views.py
+++ acme_project/catalog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def product_list(request):
+    return HttpResponse('catalog')
+def product_detail():
+    pass
+def product_category(request, category):
+    return HttpResponse(f'Категория {category}')
